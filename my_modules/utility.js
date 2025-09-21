240912e5389f51d5d13b8c0fa30a263437ea8c23
Feyi ken <feyik97@gmail.com>
2025-09-21 20:56:16 +0100

    Added the local built NodeJs module using my custom ESM format in utility file

---
+++ my_modules/utility.js
@@ -0,0 +1,7 @@
+/*Making this module to handle other logics such as:
+Check if the user input is a number,
+Check if the numbers are integers or floating points.
+*/
+
+import { add, substract, multiply, divide} from "./my_modules/calculator.js";
+
